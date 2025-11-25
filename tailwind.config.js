7a876c1396e53115cbf015a782be50c6f8319f8a
farmbasket1 <farmbasket6@gmail.com>
2025-11-25 22:26:18 +0000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0f766e',
+        accent: '#10b981'
+      }
+    }
+  },
+  plugins: [],
+}
